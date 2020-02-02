1b4a675b3b2f92b7b2c496983e63fab564135336
Max Karasinski <max@tryflux.com>
2020-02-02 18:49:06 +0000

    Format log output to be more readable

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,11 @@
 import datadog.trace.api.Trace
+import io.opentracing.Span
 import io.opentracing.util.GlobalTracer
 import kotlinx.coroutines.*
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+
+val spanRegex = Regex("""(\[ t_id=\d+, s_id=\d+, p_id=\d+]).+(ns=\d+).*""")
 
 object Main {
     private val logger: Logger = LoggerFactory.getLogger(Main.javaClass)
@@ -11,9 +14,11 @@ object Main {
     fun main(args: Array<String>) = runBlocking {
         val tracer = GlobalTracer.get()
         val span = tracer.activeSpan()
-        logger.info("${Thread.currentThread().name} $span")
+
+        logger.info("=== INIT === ${span.print()}}")
         coroutineScope {
             val deferred: MutableList<Deferred<Int>> = mutableListOf()
+            delay(1000)
             repeat(10) {
                 val newResult = async(Dispatchers.IO) {
                     val lastItem = if (it > 1) {
@@ -32,13 +37,26 @@ object Main {
     private suspend fun waitFor(millis: Long, asyncResult: Deferred<Int>?, iteration: Int) : Int {
         var tracer = GlobalTracer.get()
         var span = tracer.activeSpan()
+
         span.setTag("Iteration", iteration)
-        logger.info("${Thread.currentThread().name} $span")
+
+        logger.info("++++ IN +++: $iteration; ${span.print()}}")
+
+
         asyncResult?.await()
         tracer = GlobalTracer.get()
         span = tracer.activeSpan()
-        logger.info("${Thread.currentThread().name} $span")
+        logger.info("--- OUT ---: $iteration; ${span.print()}}")
         delay(millis)
         return 1
+    }
+}
+
+fun Span.print(): String = with(spanRegex.find(this.toString())) {
+    return if (this != null) {
+        val (a, b) = this.destructured
+        return "$a $b"
+    } else {
+        ""
     }
 }
